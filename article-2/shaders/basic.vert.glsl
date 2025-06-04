248a4ffd8d8df8b1bc90a5a2f8590007dc22155c
ostefani <ostefanishina@gmail.com>
2025-06-04 09:42:27 +0300

    Detach shaders after linking

--- article-2/shaders/basic.vert.glsl
+++ article-2/shaders/basic.vert.glsl
@@ -1,8 +1,9 @@
 #version 300 es
-layout(location = 0) in vec2 aPos;
+layout(location = 0) in vec2 aPosition;
 out vec2 vUV;
 void main() {
-    // Convert aPos from [-1,1] to [0,1] range for texture‐style UVs
-    vUV = aPos * 0.5 + 0.5;
-    gl_Position = vec4(aPos, 0.0, 1.0);
+    // Convert aPosition from [-1,1] to [0,1] range for texture‐style UVs
+    vUV = aPosition * 0.5 + 0.5;
+    vUV.y = 1.0 - vUV.y;
+    gl_Position = vec4(aPosition, 0.0, 1.0);
 }
